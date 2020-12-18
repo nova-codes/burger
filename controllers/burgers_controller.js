5a126b038d05358c2eb633aa1210c8bb4d68d3ac
Hannah Elliott <hgelliott90@gmail.com>
2020-12-18 18:54:36 -0500

    Works locally, crashes publicly.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/burgers', (req, res) => {
+router.post('/burger', (req, res) => {
     burger.insertOne(['burger_name'],[req.body.burger_name], () => {
         res.redirect('/');
     });
@@ -24,20 +24,6 @@ router.put('/burger/:id', (req, res) => {
 
     burger.updateOne({devoured: true}, condition, () => {
         res.redirect('/'); 
-    });
-});
-
-router.delete('/burgers/:id', (req, res) => {
-    let condition = 'id = ' + req.params.id;
-    console.log('condition', condition);
-
-    burger.deleteOne(condition, (result) => {
-        if(result.changedRows === 0) {
-            // if no rows were changed, the ID likely does not exist, 404 not found
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
     });
 });
 
